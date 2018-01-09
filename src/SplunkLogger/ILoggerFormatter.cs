ad31572abfb40f20216e3d74b0f3e90e15bd7387
Fabio Caldas <fabio.caldas@gmail.com>
2018-01-09 17:25:45 -0200

    Implemented ILoggerFormmater interface and SplunkJSONEntry #4

---
+++ src/SplunkLogger/ILoggerFormatter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Splunk
+{
+    public interface ILoggerFormatter
+    {
+        string Format<T>(LogLevel logLevel, EventId eventId, T state, Exception exception);
+        SplunkJSONEntry FormatJson<T>(LogLevel logLevel, EventId eventId, T state, Exception exception);
+    }
+}
---
+++ src/SplunkLogger/SplunkJSONEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Splunk
+{
+    public class SplunkJSONEntry
+    {
+        public string @event { get; private set; }
+        public ulong? time { get; private set; }
+        public string host { get; private set; }
+        public string source { get; private set; }
+        public string sourcetype { get; private set; }
+        public string index { get; private set; }
+
+        public SplunkJSONEntry(string eventText, ulong time = 0, string host = "", string source = "", string sourcetype = "", string index = "")
+        {
+            @event = eventText;
+
+            if (time > 0)
+                this.time = time;
+
+            this.host = host;
+            this.source = source;
+            this.sourcetype = sourcetype;
+            this.index = index;
+        }
+
+        public bool ShouldSerializetime() => time.HasValue && time.Value > 0;
+        public bool ShouldSerializehost() => !string.IsNullOrWhiteSpace(host);
+        public bool ShouldSerializesource() => !string.IsNullOrWhiteSpace(source);
+        public bool ShouldSerializesourcetype() => !string.IsNullOrWhiteSpace(sourcetype);
+        public bool ShouldSerializeindex() => !string.IsNullOrWhiteSpace(index);
+    }
+}
