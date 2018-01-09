8f5fbb2ab24a64b6fb1939b6fb85213fa4113407
Fabio Caldas <fabio.caldas@gmail.com>
2018-01-09 18:03:53 -0200

    Implemented Udp Socket logger class #6

---
+++ src/SplunkLogger/Loggers/UdpLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace Splunk.Loggers
+{
+    public class UdpLogger : BaseLogger, ILogger
+    {
+        readonly UdpClient udpClient;
+
+        public UdpLogger(string categoryName, LogLevel threshold, UdpClient udpClient, ILoggerFormatter loggerFormatter)
+            : base(categoryName, threshold, loggerFormatter)
+        {
+            this.udpClient = udpClient;
+        }
+
+        public void Log<T>(LogLevel logLevel, EventId eventId, T state, Exception exception, Func<T, Exception, string> formatter)
+        {
+            string formatedMessage = string.Empty;
+            if (loggerFormatter != null)
+                formatedMessage = loggerFormatter.Format(logLevel, eventId, state, exception);
+            else if (formatter != null)
+                formatedMessage = formatter(state, exception);
+
+            if (!string.IsNullOrWhiteSpace(formatedMessage))
+            {
+                formatedMessage = formatedMessage + Environment.NewLine;
+                Byte[] data = Encoding.ASCII.GetBytes(formatedMessage);
+                udpClient.Send(data, data.Length);
+            }
+        }
+    }
+}
