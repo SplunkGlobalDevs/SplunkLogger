78d855bb60ae66d7c98efac9b7edac3e705dd29a
Fabio Caldas <fabio.caldas@gmail.com>
2018-01-09 18:04:53 -0200

    Inherit from base logger class #5

--- src/SplunkLogger/Loggers/HECBaseLogger.cs
+++ src/SplunkLogger/Loggers/HECBaseLogger.cs
@@ -4,32 +4,17 @@ using Microsoft.Extensions.Logging;
                
 namespace Splunk.Loggers
 {
-    public abstract class HECBaseLogger
+    public abstract class HECBaseLogger : BaseLogger
     {
-        protected readonly ILoggerFormatter loggerFormatter;
         protected readonly BatchManager batchManager;
 
-        readonly string categoryName;
-        readonly LogLevel threshold;
         readonly HttpClient httpClient;
 
         public HECBaseLogger(string categoryName, LogLevel threshold, HttpClient httpClient, BatchManager batchManager, ILoggerFormatter loggerFormatter)
+            : base(categoryName, threshold, loggerFormatter)
         {
-            this.categoryName = categoryName;
-            this.threshold = threshold;
             this.httpClient = httpClient;
             this.batchManager = batchManager;
-            this.loggerFormatter = loggerFormatter;
-        }
-
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return (int)logLevel >= (int)threshold;
-        }
-
-        public IDisposable BeginScope<T>(T state)
-        {
-            return null;
         }
     }
 }
