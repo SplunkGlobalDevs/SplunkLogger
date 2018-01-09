d386dc6c2fb2721a8e53d573115ba23474bd987b
Fabio Caldas <fabio.caldas@gmail.com>
2018-01-09 17:49:37 -0200

    Implemented JSON Raw logger class #5

---
+++ src/SplunkLogger/Loggers/HECRawLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Splunk.Loggers
+{
+    public class HECRawLogger : HECBaseLogger, ILogger
+    {
+        public HECRawLogger(string categoryName, LogLevel threshold, HttpClient httpClient, BatchManager batchManager, ILoggerFormatter loggerFormatter)
+            : base(categoryName, threshold, httpClient, batchManager, loggerFormatter)
+        {
+        }
+
+        public void Log<T>(LogLevel logLevel, EventId eventId, T state, Exception exception, Func<T, Exception, string> formatter)
+        {
+            string formatedMessage = string.Empty;
+            if (loggerFormatter != null)
+                formatedMessage = loggerFormatter.Format(logLevel, eventId, state, exception);
+            else if (formatter != null)
+                formatedMessage = formatter(state, exception);
+
+            if (!string.IsNullOrWhiteSpace(formatedMessage))
+                batchManager.Add(formatedMessage);
+        }
+    }
+}
