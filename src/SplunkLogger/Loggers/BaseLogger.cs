fe536d02e92c083db46082b1713f2cc84264b5b0
Fabio Caldas <fabio.caldas@gmail.com>
2018-01-09 18:02:56 -0200

    Implemented base logger class #6

---
+++ src/SplunkLogger/Loggers/BaseLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Splunk.Loggers
+{
+    public abstract class BaseLogger
+    {
+        protected readonly ILoggerFormatter loggerFormatter;
+
+        readonly string categoryName;
+        readonly LogLevel threshold;
+
+        public BaseLogger(string categoryName, LogLevel threshold, ILoggerFormatter loggerFormatter)
+        {
+            this.categoryName = categoryName;
+            this.threshold = threshold;
+            this.loggerFormatter = loggerFormatter;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return (int)logLevel >= (int)threshold;
+        }
+
+        public IDisposable BeginScope<T>(T state)
+        {
+            return null;
+        }
+    }
+}
