ac5a64f5ee12c98dea0aabd569dbfe6c4581284e
Fabio Caldas <fabio.caldas@gmail.com>
2018-01-10 03:00:00 -0200

    Implemented VTEX own concept of event level #9

---
+++ src/SampleWebAPI/Logging/VTEXEventLevel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace VTEX.SampleWebAPI.Logging
+{
+    public enum VTEXEventLevel : int
+    {
+        Critical = 3,
+        Important = 2,
+        Default = 1,
+        Debug = 0,
+    }
+}
