54e9aa70a95e8cb344eb8d7daa371af088c1a7e6
Fabio Caldas <fabio.caldas@gmail.com>
2018-01-10 04:29:44 -0200

    Implement test log on default route #9

--- src/SampleWebAPI/Controllers/ValuesController.cs
+++ src/SampleWebAPI/Controllers/ValuesController.cs
@@ -1,41 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace VTEX.SampleWebAPI.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        readonly ILogger logger;
+
+        public ValuesController(ILoggerFactory loggerFactory)
+        {
+            logger = loggerFactory.CreateLogger<ValuesController>();
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            logger.DefineVTEXLog(LogLevel.Critical, 
+                                 "Values Controller", 
+                                 "api/values", 
+                                 string.Empty, 
+                                 new NotImplementedException(), 
+                                 new Tuple<string, string>("method", "GET"));
             return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
