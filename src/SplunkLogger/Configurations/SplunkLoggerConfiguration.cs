0e15746bc2f261db773e04fd594b6ceec1e1a6db
Fabio Caldas <fabio.caldas@gmail.com>
2018-01-11 19:52:21 -0200

    Cleaned class documentation

--- src/SplunkLogger/Configurations/SplunkLoggerConfiguration.cs
+++ src/SplunkLogger/Configurations/SplunkLoggerConfiguration.cs
@@ -10,19 +10,16 @@ namespace Splunk.Configurations
         /// <summary>
         /// Gets or sets the hec configuration.
         /// </summary>
-        /// <value>The hec configuration.</value>
         public HECConfiguration HecConfiguration { get; set; }
 
         /// <summary>
         /// Gets or sets the socket configuration.
         /// </summary>
-        /// <value>The socket configuration.</value>
         public SocketConfiguration SocketConfiguration { get; set; }
 
         /// <summary>
         /// Gets or sets the threshold.
         /// </summary>
-        /// <value>The threshold.</value>
         public LogLevel Threshold { get; set; } = LogLevel.Warning;
     }
 }
