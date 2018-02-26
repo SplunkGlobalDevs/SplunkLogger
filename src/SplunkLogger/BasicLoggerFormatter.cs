ad21c5535bdc9c284ef70854713669a35b8bb67d
Fabio Caldas <fabio.caldas@gmail.com>
2018-02-26 11:25:45 -0300

    Implemented default logger formatter #59

---
+++ src/SplunkLogger/BasicLoggerFormatter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Splunk
+{
+    public class BasicLoggerFormatter : ILoggerFormatter
+    {
+        public string Format<T>(string categoryName, LogLevel logLevel, EventId eventId, T state, Exception exception)
+        {
+            return string.Format("{0}: [{1}] [{2}:{3}] {4} {5}",
+                                 categoryName,
+                                 logLevel.ToString(),
+                                 eventId.Id,
+                                 eventId.Name,
+                                 state != null ? state.ToString() : string.Empty,
+                                 exception != null ? exception.ToString() : string.Empty);
+        }
+
+        public SplunkJSONEntry FormatJson<T>(string categoryName, LogLevel logLevel, EventId eventId, T state, Exception exception)
+        {
+            string eventText = Format(categoryName, logLevel, eventId, state, exception);
+            return new SplunkJSONEntry(eventText);
+        }
+    }
+}
